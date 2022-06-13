d68037f7f24eb2531edeb59d81389cbea6347144
AlphonceMutebi <60338643+AlphonceMutebi@users.noreply.github.com>
2022-06-13 19:29:10 +0300

    initial commit

---
+++ src/components/Blogs.jsx
@@ -0,0 +1,46 @@
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react'
+import { db } from '../firebaseConfig';
+
+export default function Blogs() {
+    const [blogs, setBlogs] = useState([]);
+    
+    // Fetches all blogs from the database
+    useEffect(() => {
+        const blogsRef = collection(db, "Articles" );
+        const q = query(blogsRef, orderBy("createdAt", "desc"));
+        onSnapshot(q,(snapshot) => {
+            const blogs = snapshot.docs.map((doc) =>({
+                id:doc.id,
+                ...doc.data(),
+            }));
+            setBlogs(blogs);
+            console.log(blogs);
+        });
+    }, []);
+  return (
+    <div>
+        {
+            // Display blogs from the database if there are any
+            blogs.length === 0 ? (
+                <p>No blogs yet</p>
+            ):(
+                blogs.map(({id, title,description, imageUrl, createdAt}) => (
+                <div className='border mt-3 p-3 bg-light' key={id}>
+                    <div className="row">
+                        <div className="col-3">
+                            <img src={imageUrl} alt="title" className='img-fluid' />
+                        </div>
+                        <div className='col-9 ps-3'>
+                            <h2>{title}</h2>
+                            <p>{createdAt.toDate().toString()}</p>
+                            <h4>{description}</h4>
+                        </div>
+                    </div>
+                </div>
+                )
+            )
+        )}
+    </div>
+  )
+}
---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyALFtCdNp8Tgk5SEkeJgyy3XmOADYx5q0k",
+    authDomain: "fir-33e0d.firebaseapp.com",
+    projectId: "fir-33e0d",
+    storageBucket: "fir-33e0d.appspot.com",
+    messagingSenderId: "593131978609",
+    appId: "1:593131978609:web:201c6656ad9fba5d7f40d0"
+  };
+
+const app = initializeApp(firebaseConfig);
+
+export const storage = getStorage(app);
+export const db = getFirestore(app);
